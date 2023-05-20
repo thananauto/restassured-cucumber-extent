61315da25e01c17ed8b823056dc90f5d5bc8a055
thananauto <thananjayan1988@gmail.com>
2023-05-20 16:24:23 +0530

    Added github workflows

--- src/test/java/com/qa/api/runner/RerunnerIT.java
+++ src/test/java/com/qa/api/runner/RerunnerIT.java
@@ -7,8 +7,8 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         features = {"@target/failedrerun.txt"},
         plugin = {"pretty",
-                "json:target/cucumber-json.json",
-                "tech.grasshopper.AllureCucumberMappingPlugin:target/cucumber-allure.json"},
+                "json:target/cucumber-json2.json",
+                "tech.grasshopper.AllureCucumberMappingPlugin:target/cucumber-allure2.json"},
         monochrome = true,
         dryRun = false,
         glue = {"com.qa.api.setup", "com.qa.api.stepdefinition", "com.qa.api.config", "com.qa.api.utility"}
